7d7158d09f19c98bd4423874ee0400bdb2a02ce8
shazy89 <erdo.shazy123@gmail.com>
2021-01-04 18:07:47 -0500

    style Home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
-import {connect} from 'react-redux';
+import { View, Button, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem, Text } from 'react-native-elements'
+import { connect } from 'react-redux';
 import { fetchCars } from '../actions/index'
 
 
@@ -11,19 +12,45 @@ const HomeScreen = ({ navigation, fetchCars, cars }) => {
         fetchCars()
     };
 }, []);
-      
 
-  //  console.log(cars[1])
 return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <>
        <Text>Welcome to Home screen</Text>
        <Button title="Go back" onPress={() => navigation.goBack()} />
-     </View>
+       <FlatList
+        data={cars}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={styles.item}
+              onPress={() =>
+                console.log('HEY THERE')
+              }
+            >
+              <Text style={styles.title}>{item.make}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+     </>
    );
  };
 
+
  const styles = StyleSheet.create({
 
+      item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        justifyContent: "center",
+        alignItems: "center",
+        
+      },
+      title: {
+        fontSize: 32,
+
+      },
  });
 
  const mapStateToProps = carsReducer => {
@@ -33,7 +60,8 @@ return (
   };
      
   export default connect(mapStateToProps, { fetchCars })(HomeScreen);
+
+
          
- 
 
 
