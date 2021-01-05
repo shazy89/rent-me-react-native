3585acf84c2ab97c82cd2fe8a7165d8e56b09314
shazy89 <erdo.shazy123@gmail.com>
2021-01-05 11:15:34 -0500

    display background image, add header, styling inside the home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,50 @@
 import React, { useEffect } from 'react'
-import { View, Button, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import { ListItem, Text, Avatar } from 'react-native-elements'
+import { View, Button, StyleSheet, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
+import { ListItem, Text, Avatar, Image } from 'react-native-elements'
  // import { connect } from 'react-redux';
 // import { fetchCars } from '../actions/index'
 
 
 const HomeScreen = ({ navigation }) => {
 
-
 return (
      <>
-       <Text>Welcome to Home screen</Text>
+       <ImageBackground
+          source={{ uri: 'https://res.cloudinary.com/dytheecsk/image/upload/v1609861676/img2_scqpqh.jpg' }}
+          style={styles.backgroundImage}
+          resizeMode='cover'
+        />
+        <View style={styles.heading}>
+           <Text h2 style={styles.headingText}> Reserve A Vehicle</Text>
+        </View>
      </>
    );
  };
+
+            
+ const styles = StyleSheet.create({
+     backgroundImage: {
+        width: '100%', 
+        height: 300
+     },
+     heading: {
+        marginTop: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#d3d3d3',
+        borderWidth: 1
+     },
+     headingText: {
+        fontFamily: "Cochin" 
+     }
+ });
+     
   
 
 
- const styles = StyleSheet.create({
- });
 
      
   export default HomeScreen;
 
-
-       
 
 
