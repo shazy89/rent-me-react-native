e78926e55d3bab25203a7c947a5e580f63561087
shazy89 <erdo.shazy123@gmail.com>
2021-01-07 16:45:52 -0500

    added style to the AvailableCarsScreen

--- src/components/AvailableCarsDetail.js
+++ src/components/AvailableCarsDetail.js
@@ -4,32 +4,67 @@ import { Text, StyleSheet, View, Image } from "react-native";
 
 
 const AvailableCarsDetail = ({result}) => {
-
+console.log(result)
    return (
        <View style={styles.container}> 
              <Image style={styles.img} source={{uri: result.img}} />
-             <Text style={styles.name}>{result.make}</Text>
+            <Text style={styles.textContainer}>
+             <Text style={styles.titleText}>{result.make} 
+               {"\n"}
+     
+             </Text>
+             <Text style={styles.titleText}> {result.model}
+               {"\n"}
+             </Text>
+             <Text style={styles.priceText}> ${result.rentPrice}
+               {"\n"}
+             </Text>
+         </Text>   
+            
        </View>
    );
 };
-  
 
 const styles = StyleSheet.create({
     container: {
         marginLeft: 15,
-    },
-    img: {
-        width: 250,
+        borderWidth: 1,
         borderRadius: 4,
-        height: 120,
-        marginBottom: 5
-      },
-      name: {
+        borderBottomColor: 'black',
+        width: 340,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        shadowColor: '#000000',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1.0
+    },
+      img: {
+          width: 200,
+          borderRadius: 4,
+          height: 120,
+          marginBottom: 5
+        },
+        textContainer: {
+          fontFamily: "Cochin"
+        },
+        titleText: {
+          fontSize: 16,
+          fontWeight: "bold"
+        },
+        priceText: {
           fontWeight: 'bold',
-          
-      }
-})
+          fontSize: 20,
+        }
+  })
+     
 
 export default AvailableCarsDetail;
+            
+  
+
     
       
