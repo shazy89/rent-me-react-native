7f1e212ae7b9ab427b46b6b1b9592fc0535c2e5b
shazy89 <erdo.shazy123@gmail.com>
2021-01-09 08:05:20 -0500

    added random method to the inventory screen

--- src/screens/InventoryScreen.js
+++ src/screens/InventoryScreen.js
@@ -4,26 +4,51 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { connect } from 'react-redux';
 
 const InventoryScreen = ({cars}) => {
+const randomCars = cars.map(x => [Math.random(), x]).sort(([a], [b]) => a - b).map(([_, x]) => x);
+
 
   return (
     <SafeAreaView >
-     <ScrollView>
-       <View  style={styles.container}>
-           <Text>HEY</Text>
-       </View>
-     </ScrollView>   
+           <FlatList
+            data={randomCars}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => {
+              return (
+                <ScrollView>   
+                    <View style={styles.cardView}> 
+                      <Text style={styles.boxStyle}>{item.make}</Text>
+                   </View> 
+               </ScrollView>   
+            );
+          }}
+      />
     </SafeAreaView>
   );
 };
+            
 
+    
 const styles = StyleSheet.create({
-   container: {
-    flex: 1
-   }
+   boxStyle: {
+    alignSelf: 'center',   
+    height: 160, 
+    width: 320, 
+    borderWidth: 1, 
+    marginBottom: 5
+   },
 });
+    
+   
+ 
+
+
+       
 const mapStateToProps = store => {
     return {
         cars: store.cars.cars,
     };
  };
 export default connect(mapStateToProps)(InventoryScreen);
+
+
+ 
